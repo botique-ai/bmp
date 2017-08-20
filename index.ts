791e4be419a93d82a6cae11565397f78bf851c5a
Pavel Tarno <paveltarno@gmail.com>
2017-08-20 18:53:35 +0300

    added types

--- index.ts
+++ index.ts
@@ -1,3 +1,45 @@
-interface test{
-  foo: number,
+export interface BotiqueMessage {
+  originId: string;
+  bot: BotPlatformData;
+}
+
+export enum ContentType {
+  Text,
+  Payload,
+  Notification,
+  Attachment,
+  Link
+}
+
+export interface BotPlatformData {
+  _id: string | any;
+  name: string;
+  platformData: { [key: number]: any }; // the key is a platform type
+}
+
+export interface UserMessage extends BotiqueMessage {
+  dateReceived: Date;
+  isEcho: boolean;
+  userId: string;
+  metadata?: any;
+  contentType: ContentType;
+  isAdminMessage?: boolean;
+  content: {
+    text?: string;
+    link?: string;
+    payload?: string;
+    attachments?: Array<UserMessageAttachment>;
+  };
+}
+
+export interface UserMessageAttachment {
+  type?: "audio" | "fallback" | "file" | "image" | "location" | "video";
+  payload?: {
+    url?: string;
+    coordinates?: {
+      // Coordinates will be send for att type 'location'
+      lat: Number;
+      long: Number;
+    };
+  };
 }
