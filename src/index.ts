065a67ff50e548534bab2d045c81d3fcce1e5f92
Pavel Tarno <paveltarno@gmail.com>
2017-08-31 15:14:25 +0300

    moved index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// Types
+export * from "./types/BotiqueMessage";
+export * from "./types/BotMessage";
+export * from "./types/BotPlatformData";
+export * from "./types/BotPlatformType";
+export * from "./types/ChatUserProfile";
+export * from "./types/CollectedParams";
+export * from "./types/Gender";
+export * from "./types/TargetIntent";
+export * from "./types/TargetParameter";
+export * from "./types/UserConversation";
+export * from "./types/UserMessage";
+export * from "./types/BmpBot";
+
+// Adapters
+import * as DirectLineAdapter from "./adapters/DirectLineAdapter";
+export { DirectLineAdapter };
