39aba1b1685e8faf6171f445f29b9aaf8bff9acb
Pavel Tarno <paveltarno@gmail.com>
2017-08-31 15:06:38 +0300

    added bmp bot to exports

--- index.ts
+++ index.ts
@@ -3,7 +3,6 @@ export * from "./src/types/BotiqueMessage";
 export * from "./src/types/BotMessage";
 export * from "./src/types/BotPlatformData";
 export * from "./src/types/BotPlatformType";
-export * from "./src/types/ChatUser";
 export * from "./src/types/ChatUserProfile";
 export * from "./src/types/CollectedParams";
 export * from "./src/types/Gender";
@@ -11,7 +10,8 @@ export * from "./src/types/TargetIntent";
 export * from "./src/types/TargetParameter";
 export * from "./src/types/UserConversation";
 export * from "./src/types/UserMessage";
+export * from "./src/types/BmpBot";
 
 // Adapters
-import * as DirectLineAdapter from './src/adapters/DirectLineAdapter';
+import * as DirectLineAdapter from "./src/adapters/DirectLineAdapter";
 export { DirectLineAdapter };
